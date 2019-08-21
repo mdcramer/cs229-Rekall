11de41ade54afa032070f81cb6edc93dc97c02ca
Dan Fu <danfu09@gmail.com>
2019-08-20 17:17:15 -0700

    Update multiple seeds for conversations'

--- conversation_experiments/run_seeds.sh
+++ conversation_experiments/run_seeds.sh
@@ -3,8 +3,8 @@ echo $SEED
 
 mkdir -p results_$SEED
 
-./train_model.sh $SEED
-./test_model.sh $SEED
+./train_3d_model.sh $SEED
+./test_3d_model.sh $SEED
 
 # Evaluate results...
 python process_results.py $SEED >> seed_results.log
