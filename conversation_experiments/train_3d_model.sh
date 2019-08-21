d66b9f4bf3e627f3014af62262644e6a369cce37
Dan Fu <danfu09@gmail.com>
2019-08-20 17:14:04 -0700

    3D scripts for conversations

---
+++ conversation_experiments/train_3d_model.sh
@@ -0,0 +1,9 @@
+SEED=$1
+pwd
+cd 3D-ResNets-PyTorch
+python main.py --root_path ~/rekall_experiments/conversation_experiments \
+--video_path conversation_export/images --annotation_path conversation_export/data \
+--result_path results_$SEED --dataset conversations --n_classes 400 --n_finetune_classes 2 \
+--pretrain_path models/resnet-34-kinetics.pth --ft_begin_index 4 \
+--model resnet --model_depth 34 --resnet_shortcut A --batch_size 32 --n_threads 16 --checkpoint 5 \
+--no_val --manual_seed $SEED
