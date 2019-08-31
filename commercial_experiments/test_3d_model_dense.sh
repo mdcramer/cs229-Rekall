7b673dee4ac243addc7d2f8accfd7d2bef8e2928
Dan Fu <danfu09@gmail.com>
2019-08-30 18:07:09 -0700

    Scripts for commercials dense

---
+++ commercial_experiments/test_3d_model_dense.sh
@@ -0,0 +1,10 @@
+SEED=$1
+pwd
+cd 3D-ResNets-PyTorch
+python main.py --root_path ~/rekall_experiments/commercial_experiments \
+--video_path commercials_dense/images --annotation_path commercials_dense/data \
+--result_path results_dense_$SEED --dataset dense_sample --n_classes 2 --n_finetune_classes 2 \
+--resume_path results_dense_$SEED/save_200.pth --ft_begin_index 4 \
+--model resnet --model_depth 34 --resnet_shortcut A --batch_size 32 --n_threads 16 --checkpoint 5 \
+--no_val --no_train --test --test_subset test
+
---
+++ commercial_experiments/train_3d_model_dense.sh
@@ -0,0 +1,9 @@
+SEED=$1
+pwd
+cd 3D-ResNets-PyTorch
+python main.py --root_path ~/rekall_experiments/commercial_experiments \
+--video_path commercials_dense/images --annotation_path commercials_dense/data \
+--result_path results_dense_$SEED --dataset dense_sample --n_classes 400 --n_finetune_classes 2 \
+--pretrain_path models/resnet-34-kinetics.pth --ft_begin_index 4 \
+--model resnet --model_depth 34 --resnet_shortcut A --batch_size 32 --n_threads 16 --checkpoint 5 \
+--no_val --manual_seed $SEED
