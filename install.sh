d13eeac5f8c511eb5089da955bc809c38671f146
Sundararajan Renganathan <rsundar@stanford.edu>
2019-11-27 00:01:08 +0000

    Shell script to install all the dependencies on Ubuntu 16.04

---
+++ install.sh
@@ -0,0 +1,20 @@
+# Cuda installation only for Ubuntu 16.04!
+curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda
+
+# Install miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+
+# Install Pytorch and some other stuff in the base Conda environment
+conda activate base
+conda install jupyter
+conda install ipython
+conda install pytorch torchvision cudatoolkit=10.1 -c pytorch
+
+# Install other utilities
+sudo apt install tig
+
